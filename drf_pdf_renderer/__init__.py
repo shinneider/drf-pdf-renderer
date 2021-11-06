933d9a2069531177c80198857ea2be37ce885cd3
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2021-11-06 17:21:24 -0300

    fix apps import

--- drf_pdf_renderer/__init__.py
+++ drf_pdf_renderer/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     django = None
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
     default_app_config = 'drf_pdf_renderer.apps.DjangoPdfRendererConfig'
--- drf_pdf_renderer/apps.py
+++ drf_pdf_renderer/apps.py
@@ -4,4 +4,5 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class DjangoPdfRendererConfig(AppConfig):  # Our app config class
+    name = 'drf_pdf_renderer'
     verbose_name = _('Django PDF Renderer')
